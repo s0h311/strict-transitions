a7a4ce0e6d6858b1d832c837ec2f3ad1209af0d6
Soheil Nazari <kontakt@soheilnazari.de>
2024-12-21 16:53:16 +0100

    install ngrx for angular playground

--- playground/angular/src/app/app.component.ts
+++ playground/angular/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core'
 import { RouterOutlet } from '@angular/router'
+import { Store } from '@ngrx/store'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,6 @@ import { RouterOutlet } from '@angular/router'
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor(private store: Store) {}
+}
